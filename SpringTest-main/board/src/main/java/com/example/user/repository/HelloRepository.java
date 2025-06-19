93c4924bcc1c9c93b3c58c6ece0df5f27a8efacf
AquaRiva <hmk0676@naver.com>
2025-06-19 16:22:46 +0900

    reset

--- SpringTest-main/board/src/main/java/com/example/user/repository/HelloRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.repository;
-
-import com.example.user.entity.HelloEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HelloRepository extends JpaRepository<HelloEntity, Long> {
-}
